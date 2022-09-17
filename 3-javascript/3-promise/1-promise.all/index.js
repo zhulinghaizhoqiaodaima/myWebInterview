1690cf2dff4d4cbddc9d2feaa3196d7392cbb827
zbt <2479879758@qq.com>
2022-09-17 16:32:15 +0800

    原型链

--- 3-javascript/3-promise/1-promise.all/index.js
+++ 3-javascript/3-promise/1-promise.all/index.js
@@ -1,13 +1,26 @@
-Promise.MyAll = (promises)=>{
-    const arr = [];
-    return new Promise((resolve,reject)=>{
-        for (const promise of promises) {
-            Promise.resolve(promise).then(res=>{
-                arr.push(res)
-                if(Object.keys(arr).length === promises.length) return resolve(arr) // 全部通过
-            }).catch(reject)
-        }
-    })
+// Promise.MyAll = (promises)=>{
+//     const arr = [];
+//     return new Promise((resolve,reject)=>{
+//         for (const promise of promises) {
+//             Promise.resolve(promise).then(res=>{
+//                 arr.push(res)
+//                 if(Object.keys(arr).length === promises.length) return resolve(arr) // 全部通过
+//             }).catch(reject)
+//         }
+//     })
+// }
+
+Promise.myAll = (promises)=>{
+  return new Promise((resolve,reject)=>{
+    let arr = [];
+    for (const item of promises) {
+        Promise.resolve(item).then(res=>{ // 这里是微队列代码
+            arr.push(res)
+          if(Object.keys(promises).length === arr.length) return resolve(arr)
+        }).catch(reject)
+    }
+    // 同步代码，执行一次，arr=[]
+  })
 }
 
 const p1 = Promise.resolve('p1')
@@ -22,7 +35,7 @@ const p3 = new Promise((resolve, reject) => {
   }, 2000)
 })
 
-const p4 = Promise.reject('p4 rejected')
+// const p4 = Promise.reject('p4 rejected')
 
 // const p5 = new Promise((resolve, reject) => {
 //   setTimeout(() => {
@@ -32,6 +45,6 @@ const p4 = Promise.reject('p4 rejected')
 
 
 // 两个失败的 Promise
-Promise.MyAll([p1, p2, p4])
+Promise.myAll([p1, p2, p3])
   .then(res => console.log(res))
   .catch(err => console.log(err)) // p4 rejected
